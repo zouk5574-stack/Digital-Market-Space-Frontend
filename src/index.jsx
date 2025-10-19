9b3d572ad0ced6d8c7c9c9a8c60e445e8bd3f3c6
zouk5574-stack <zouk5574@gmail.com>
2025-10-19 09:48:43 +0100

    Add main entry point for React application

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+// src/index.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AuthProvider } from "./contexts/AuthContext";
+import "./index.css";
+import "./App.css";
+import { Toaster } from "react-hot-toast";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+        <Toaster position="top-right" />
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
+);
