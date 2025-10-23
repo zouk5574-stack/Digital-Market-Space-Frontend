82ba70d15c5f4bfc299dcc41b97a9201a2d58ca4
zouk5574-stack <zouk5574@gmail.com>
2025-10-23 09:04:21 +0100

    Create users.js

---
+++ src/utils/endpoints/users.js
@@ -0,0 +1,7 @@
+export const USER_ENDPOINTS = {
+  LIST: '/api/users',
+  DETAIL: id => `/api/users/${id}`,
+  UPDATE: id => `/api/users/${id}`,
+  DELETE: id => `/api/users/${id}`,
+  PROFILE: '/api/users/profile',
+};
