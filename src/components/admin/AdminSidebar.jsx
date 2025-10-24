59636316acfe59d0606a59af92efcbffcc34f28a
zouk5574-stack <zouk5574@gmail.com>
2025-10-24 07:22:07 +0100

    Create AdminSidebar.jsx

---
+++ src/components/admin/AdminSidebar.jsx
@@ -0,0 +1,9 @@
+// Ajoutez cet item dans le menu de navigation :
+import { Category } from '@mui/icons-material';
+
+// Dans la liste des liens de navigation :
+{
+  icon: <Category />,
+  text: 'Catégories',
+  path: '/admin/categories'
+}
